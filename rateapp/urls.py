37fca429de2165c4e47938b75e50f6c6e63527dc
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-17 17:47:13 +0300

    add upload project form

--- rateapp/urls.py
+++ rateapp/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url('^$',views.index, name = 'index'),
+    path('',views.index, name = 'index'),
+    path('profile/', views.profile, name= 'profile'),
+    path(r'^search/', views.search, name='search_results'),
+   path('uploadproject/',views.new_project,name='new_project')
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- rateapp/views.py
+++ rateapp/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from rateapp.forms import ProjectForm
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse
+from.models import Profile, Project
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    projects = Project.objects.all()
+    return render(request, 'index.html', {'projects': projects})
+
+def profile(request):
+    user = request.user
+    profile = Profile.objects.filter(user=user)
+    projects = Profile.objects.filter(user=user)
+    return render(request, 'profile.html',{'user':user, 'profile': profile, 'projects':projects})
+
+def search(request):
+
+    if 'projectname' in request.GET and request.GET ['projectname']:
+        search_title = request.GET.get('projectname')
+        searched_project = Project.search_project(search_title)
+
+        message = f"{search_title}"
+
+        return render(request, 'search.html', {"message": message, "projects": searched_project })
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+def  new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST or None, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect('index')
+    else:
+        form = ProjectForm()
+    return render(request,'newproject.html',{"form":form})
