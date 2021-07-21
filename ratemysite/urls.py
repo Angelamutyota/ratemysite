928d42ea88886a650c5705911f0307ee33a38736
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-21 18:14:12 +0300

    edit star ratings url pattern

--- ratemysite/urls.py
+++ ratemysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('rateapp.urls')),
-    path('ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
 
 ]
